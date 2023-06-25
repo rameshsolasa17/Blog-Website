3cdd843a7bcd95cce88752e2fc1599634004ed59
Solasa Venkata Naga Ramesh <82230347+rameshsolasa17@users.noreply.github.com>
2023-06-25 10:42:52 +0530

    Update app.js

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost:27017/blogDB", {useNewUrlParser: true});
+mongoose.connect("mongodb+srv://rameshsolasa2001:rameshsolasa@cluster0.tdxhysz.mongodb.net/?retryWrites=true&w=majority", {useNewUrlParser: true});
 
 const postSchema = {
   title: String,
