b9b2e6c5a2ebfddfb2efc58bec4f2ae8d034a37c
Solasa Venkata Naga Ramesh <82230347+rameshsolasa17@users.noreply.github.com>
2023-06-25 12:02:51 +0530

    Update app.js

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rho
 const app = express();
 
 app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "views"));
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
